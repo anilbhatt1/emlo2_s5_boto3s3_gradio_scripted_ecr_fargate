1b577b9de5c78ee937f67cc6976c6fc2a15088f5
anilbhatt1 <anilaknb@gmail.com>
2023-09-26 15:45:49 +0530

    Added s3 files

--- dockerize/inference.py
+++ dockerize/inference.py
@@ -3,8 +3,13 @@ import boto3
 import gradio as gr
 from torchvision import transforms as T
 import argparse
+import logging
+import os
 
-default_model_path  = "s3://emlo2s5/model.script.pt"
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+default_model_path  = "s3://emlo2s5/model_weights/model.script.pt"
 default_output_path = "s3://emlo2s5/outputs"
 
 parser = argparse.ArgumentParser(description='Docker based Inferencing')
@@ -17,7 +22,7 @@ parser.add_argument('-f', '--flagged-dir', type=str, default=default_output_path
 def download_model(s3_bucket_path):
     # Initialize an S3 client
     s3 = boto3.client('s3')
-
+    logger.info(f'Inside download model s3_bucket_path : {s3_bucket_path}')
     # Specify the bucket name and file name
     lst = s3_bucket_path.split('/')
     model_file_name = lst[-1]
@@ -27,18 +32,38 @@ def download_model(s3_bucket_path):
 
     # Specify the local path where you want to save the downloaded file
     local_path = './' + model_file_name
+    
+    logger.info(f'bucket_name is {bucket_name}')
+    logger.info(f's3_file_path is {s3_file_path}')
+    logger.info(f'local_path is {local_path}')  
+    logger.info(f'model_file_name is {model_file_name}') 
 
     # Use the S3 client to download the file
     try:
         s3.download_file(bucket_name, s3_file_path, local_path)
+        logger.info(f'Successfully downloaded {model_file_name} to {local_path}')
         print(f'Successfully downloaded {model_file_name} to {local_path}')
     except Exception as e:
-        print(f'Error: {str(e)}')
+        logger.info(f'S3 download Error: {str(e)}')
+        print(f'S3 download Error: {str(e)}')
 
+    current_directory = os.getcwd()
+    logger.info(f'Inside download model -Current dir is {current_directory}')
+    directory_contents = os.listdir(current_directory)
+    for item in directory_contents:
+        logger.info(f'--**item is {item}')
+        
     return model_file_name
 
 def demo(model_file_name):
-    model = torch.jit.load(model_file_name)  
+    current_directory = os.getcwd()
+    logger.info(f'Current dir is {current_directory}')
+    directory_contents = os.listdir(current_directory)
+    for item in directory_contents:
+        logger.info(f'--item is {item}')
+    model_file_path = './' + model_file_name
+    logger.info(f'Model_file_path is {model_file_path}')
+    model = torch.jit.load(model_file_path)  
 
     class_name = ['airplane', 'car','birds', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
@@ -62,7 +87,11 @@ def demo(model_file_name):
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    logger.info(f'Args : {args}')
     s3_bucket_path = args.model   
+    logger.info(f' Args s3_bucket_path  : {s3_bucket_path}')
     output_path = args.flagged_dir
+    logger.info(f' Args output_path  : {output_path}')
     model_file = download_model(s3_bucket_path)
+    logger.info(f' After download model_file  : {model_file}')
     demo(model_file)
